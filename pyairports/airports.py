0426ac99f680ff81bdb25d908c2e5b0da5d11bb5
Michael Klamkin <klamike@users.noreply.github.com>
2019-11-18 08:29:40 -0500

    Update airports.py

--- pyairports/airports.py
+++ pyairports/airports.py
@@ -47,6 +47,8 @@ class Airports(object):
 
     @staticmethod
     def _validate(iata):
+        if not isinstance(iata, (str)):
+            raise ValueError("iata must be a string, it is a {0}".format(type(iata)))
         iata = iata.strip().upper()
         if not len(iata) == 3:
             raise ValueError("iata must be three characters")
